95e44ab05b0e9b2a540ea7fcb2314acca998b820
Zentelechia <zentelechia@gmail.com>
2015-11-13 15:19:19 +0300

    1

--- server/router.js
+++ server/router.js
@@ -6,6 +6,8 @@ Router.route('/mail', function () {
   var res = this.response;
   var req = this.request;
   	console.log(req.params);
+  	console.log(req.body);
+
     notifications.insert({text : 'new', new : true, timestamp:  Date.now()});
     res.end('');
 }, {where: 'server'});
