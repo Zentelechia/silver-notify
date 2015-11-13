9069da98f09fd4cb51b6eb8224f741efb4ffe55a
Zentelechia <zentelechia@gmail.com>
2015-11-13 15:21:10 +0300

    1

--- server/router.js
+++ server/router.js
@@ -5,10 +5,10 @@ Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
 Router.route('/mail', function () {
   var res = this.response;
   var req = this.request;
-  	console.log(req.params);
+  
   	console.log(req.body);
 
-    notifications.insert({text : 'new', new : true, timestamp:  Date.now()});
+    notifications.insert({text : req.body.body-plain, new : true, timestamp:  Date.now()});
     res.end('');
 }, {where: 'server'});
 
