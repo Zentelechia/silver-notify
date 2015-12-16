fb94ff0e94dcfc2a4bbd6a012fd0c908c2eb8d20
Zentelechia <zentelechia@gmail.com>
2015-12-16 12:18:10 +0300

    1

--- server/router.js
+++ server/router.js
@@ -7,8 +7,13 @@ Router.route('/mail', function () {
   var req = this.request;
   
   	console.log(req.body);
+  	msg=JSON.parse(req.body["body-plain"]);
 
-    notifications.insert({text : req.body["body-plain"], new : true, timestamp:  Date.now()});
+    //notifications.insert({text : req.body["body-plain"], new : true, timestamp:  Date.now()});
+    msg.text=msg.message;
+    msg.timestamp=Date.now();
+    msg.new=true;
+    notifications.insert(msg);
     res.end('');
 }, {where: 'server'});
 
