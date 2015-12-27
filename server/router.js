65be03814bd1c11348ba0d732214da2389752249
Zentelechia <zentelechia@gmail.com>
2015-12-27 23:41:10 +0300

    1

--- server/router.js
+++ server/router.js
@@ -6,8 +6,9 @@ Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
 Router.route('/mail', function () {
   var res = this.response;
   var req = this.request;
-  var msg= {};  
+  
 
+  var msg= {};  
   msg.new=true;
   msg.timestamp=Date.now();
   msg.message="Новое событие";
@@ -25,6 +26,7 @@ Router.route('/mail', function () {
   
 try {
       message=JSON.parse(req.body["body-plain"]);
+      console.log(message);
       gosnomer=message.gosnomer;
       unit_id=get_unit_id_by_gosnomer(gosnomer);
       time_start=message.time_start;
@@ -32,31 +34,28 @@ try {
       to=moment(time_start,"DD.MM.YYYY HH:mm:ss Z");
       from=moment(time_start,"DD.MM.YYYY HH:mm:ss Z").subtract(60,'m');
       format_string=("YYYY-MM-DDThh:mm:ss.000");
-      msg.text=body.message;  
-      
+      msg.text=message.message;        
       if(msg.message.search("тревожной")!=-1){
-      stats=get_discrete_statistics(unit_id,from.format(format_string),to.format(format_string));
-      console.log(stats);
-      ports=find_actuated_discrete_sensor(stats, from.unix());
-      if (ports.length>0){
-        ports.forEach(function(pp){
-            pp=terminal_settings.findOne({code: "P"+pp});
-            msg.text=pp.value;
-            notifications.insert(msg);            
-
-        });
-      }
-      else{
-        notifications.insert(msg);
-      }
-
+        stats=get_discrete_statistics(unit_id,from.format(format_string),to.format(format_string));
+        console.log(stats);
+        ports=find_actuated_discrete_sensor(stats, from.unix());
+        if (ports.length>0){
+          ports.forEach(function(pp){
+              p=terminal_settings.findOne({code: "P"+pp});
+              msg.text=p.Value;
+              notifications.insert(msg);            
+          });
+        }
+        else{
+          notifications.insert(msg);
+        }
       }
       console.log(port);
     //find_actuated_discrete_sensor(stats, from.unix());
-    msg.text=body.message;
  }
   catch(err){
-    console.log(err);
+
+    console.log("catch err:" + err);
  }
   res.end('');
 }, {where: 'server'});
