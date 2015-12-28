a82a005c0f13ecd9bf5790623c90cef886c00267
Zentelechia <zentelechia@gmail.com>
2015-12-28 06:45:50 +0300

    1

--- server/router.js
+++ server/router.js
@@ -55,7 +55,7 @@ Router.route('/mail', function () {
         if (ports.length>0){
           ports.forEach(function(pp,i){
               p=terminal_settings.findOne({code: "P"+pp});
-              msg.text=p.value+"("+msg.gosnomer+")";
+              msg.text=p.value+" ("+msg.gosnomer+")";
               notifications.insert(msg);            
           });
         }
